43432dde38078e44a4e83b1fa2805a11ca2999bf
MinerFarmIsCool <joshua_whelan@outlook.com>
2026-04-29 17:31:09 +1000

    Event editing and deleting!

--- Super_Calendar/SCalendar/urls.py
+++ Super_Calendar/SCalendar/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('create_event/', views.create_event, name='create_event'),
+    path('event/<int:event_id>/delete', views.delete_event, name='delete_event'),
+    path('event/<int:event_id>/edit', views.edit_event, name='edit_event'),
     path('event_list/', views.event_list, name='event_list'),
 ]
--- Super_Calendar/SCalendar/views.py
+++ Super_Calendar/SCalendar/views.py
@@ -27,3 +27,27 @@ def create_event(request):
 def event_list(request):
     events = Event.objects.all()
     return render(request, "SCalendar/event_list.html", {"events": events})
+
+def edit_event(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    if request.user.profile != event.profile:
+        return redirect("SCalendar:home")
+    if request.method == "POST":
+        form = EventForm(request.POST, profile=request.user.profile, instance=event)
+        if form.is_valid():
+            event = form.save()
+            messages.success(request, f"Event '{event.name}' updated successfully.")
+            return redirect("SCalendar:home")
+    else:
+        form = EventForm(profile=request.user.profile, instance=event)
+    return render(request, "SCalendar/create_event.html", {"form": form})
+
+@login_required(login_url="users:login")
+def delete_event(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    if request.user.profile == event.profile:
+        event.delete()
+        messages.success(request, f"Event '{event.name}' deleted successfully.")
+    else:
+        messages.error(request, f"Event '{event.name}' not deleted. You are not allowed to delete this event.")
+    return redirect("SCalendar:home")
